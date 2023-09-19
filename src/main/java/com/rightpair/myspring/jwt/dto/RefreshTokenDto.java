b3addde9e13d8fae3ac5b3bd411cbb6b0bf6d62f
rightpair <tkddn204@gmail.com>
2023-09-19 17:49:50 +0900

    feat: `RefreshTokenDto`에 validation 추가
    - 요청 필드에 Pattern 추가
    
    Signed-off-by: rightpair <tkddn204@gmail.com>

--- src/main/java/com/rightpair/myspring/jwt/dto/RefreshTokenDto.java
+++ src/main/java/com/rightpair/myspring/jwt/dto/RefreshTokenDto.java
@@ -1,5 +1,6 @@
 package com.rightpair.myspring.jwt.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 
 public class RefreshTokenDto {
@@ -7,7 +8,9 @@ public class RefreshTokenDto {
   @Builder
   public record RefreshRequest(
       Long memberId,
+      @Pattern(regexp = "Bearer")
       String grantType,
+      @Pattern(regexp = "^[\\w-]*\\.[\\w-]*\\.[\\w-]*$", message = "올바른 JWT 포맷이 아닙니다.")
       String refreshToken
   ) {
   }
